9b6649bf678b5cc5b83afffc82a818140c4be589
Sona30k <146016113+Sona30k@users.noreply.github.com>
2024-06-05 06:44:00 +0000

    operators

---
+++ 1.js
@@ -0,0 +1,18 @@
+//operators
+//airthmetic
+
+console.log(2+5);
+console.log(6-4);
+console.log(9*8);
+console.log(9**2);
+let a=1;
+console.log(++a);
+
+//comparison
+
+a=5;
+b=8;
+console.log(a==b);
+console.log(a!=b);
+console.log(a>=b);
+console.log(a<=b);
