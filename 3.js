f588b34acef85b8a6c6be931efcaf3ad4b3cd13a
Sona30k <146016113+Sona30k@users.noreply.github.com>
2024-06-05 07:42:50 +0000

    loop

---
+++ 3.js
@@ -0,0 +1,25 @@
+//loop
+for (var i=0 ; i<=10;i++){
+    console.log("hello");
+    }
+for(var i=1; i<=10 ; i++){
+    console.log("2 x" +i+ "=" +2*i) 
+
+    }
+for(var i=1; i<=10 ; i++){
+    console.log(`2 x ${i} = ${2*i}`) //backtics
+    
+    }
+
+//while
+ var i=1;
+ while(i<=10){
+    console.log(`2 x ${i} = ${2*i}`);
+    i++;
+    }
+//do while
+var i=1;
+do{
+    console.log(`2 x ${i} = ${2*i}`);
+    i++;
+    }while(i<=10)
