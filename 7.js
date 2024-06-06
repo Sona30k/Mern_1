d622038f40ef066664eaa1ea1ccd343ced587d26
Sona30k <146016113+Sona30k@users.noreply.github.com>
2024-06-06 06:55:42 +0000

    multi-d array

---
+++ 7.js
@@ -0,0 +1,20 @@
+//multideimensional array
+
+let a=[
+    ["sona",19],
+    ["ashu",21],
+    ["dak",22]
+]
+/*console.log(a[1]);
+console.log(a[2][0]);
+for(i=0;i<=a.length;i++){
+    for(let j=0;j<a[i].length;j++){
+    console.log(a[i][j]);}
+
+}*/
+a.forEach(val =>{
+    console.log(val);
+})
+ for(let b of a){
+    console.log(b)
+ }
