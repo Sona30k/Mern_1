1373b74e885716a35a57a9babff489ac40e17bef
Sona30k <146016113+Sona30k@users.noreply.github.com>
2024-06-06 07:12:35 +0000

    destructing

--- 8.js
+++ 8.js
@@ -11,3 +11,19 @@ let ar2=[...ar1]//spread operator
 ar2.push("fuckyou");
 console.log(ar1);
 console.log(ar2);
+
+//array destructing
+
+let are1=["sde","google",25,"soon","goal"]
+let item1=are1[0];
+let item2=are1[1];
+let item3=are1[2];
+console.log(item1);
+console.log(item2);
+console.log(item3);
+
+let[ a,b,c,...d]=are1;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
