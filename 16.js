a9da13a07f3c543778446099604a7f7c8bc3e7fa
Sona30k <146016113+Sona30k@users.noreply.github.com>
2024-06-08 06:33:51 +0000

    ascynchronous

---
+++ 16.js
@@ -0,0 +1,24 @@
+//js is synchronous runs one by one
+
+//asynchronous
+console.log("this")
+console.log("is")
+console.log("orange cat")
+
+//set time out
+
+console.log("heloww");
+setTimeout(() =>{
+    console.log("meow")// asynchronous
+
+},5000)
+
+console.log("pawlice")
+
+
+setTimeout(fun,2000)
+console.log("lana");
+function fun(){
+    console.log("del")
+}
+console.log("rey")
